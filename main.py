9bb0404eaf75f6a8ae6fb60ecbe196c405ba65c0
H3VO <hevonox@gmail.com>
2024-12-05 21:42:57 -0600

    terminamos las funciones de carga a la base de datos y ejecucion de consultas SQL

--- main.py
+++ main.py
@@ -79,10 +79,14 @@ def load_to_csv(df, output_path):
 def load_to_db(df, sql_connection, table_name):
     ''' This function saves the final data frame to a database
     table with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
 def run_query(query_statement, sql_connection):
     ''' This function runs the query on the database table and
     prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_oupout=  pd.read_sql(query_statement,sql_connection)
+    print(query_oupout)
 
 ''' Here, you define the required entities and call the relevant
 functions in the correct order to complete the project. Note that this
